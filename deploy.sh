d93fdfb3f72cb002d2b77280c893deef6ec70347
Benjamin Zaslavsky <bzaslavsky@omnilog.fr>
2017-03-23 11:52:58 +0100

    Adding deploy script, test for travis-ci

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+lastCommit=$(git log --format="%H" -n 1)
+echo "Commit : $lastCommit"
+
+filesChanged=$(git diff-tree --no-commit-id --name-only -r $lastCommit)
+if [ ${#filesChanged[@]} -eq 0 ]; then
+    echo "No files to update"
+else
+    for f in $filesChanged
+	do
+		#do not upload these files that aren't necessary to the site
+		if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != ".gitignore" ]
+		then
+	 		echo "Uploading $f"
+	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS $FTP_HOST$f
+		fi
+	done
+fi
