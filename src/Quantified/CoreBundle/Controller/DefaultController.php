88362af9ba4ba76bd0c5d3a3ee4296776c61e946
Benjamin Zaslavsky <bzaslavsky@omnilog.fr>
2017-03-23 14:02:13 +0100

    Adding docblocks for test deploy

--- src/Quantified/CoreBundle/Controller/DefaultController.php
+++ src/Quantified/CoreBundle/Controller/DefaultController.php
@@ -4,8 +4,15 @@ namespace Quantified\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Class DefaultController
+ * @package Quantified\CoreBundle\Controller
+ */
 class DefaultController extends Controller
 {
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function soonAction()
     {
         return $this->render('CoreBundle:Default:soon.html.twig');
