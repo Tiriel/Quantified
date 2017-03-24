4ab3e0f93d08354e3d0e7d50f9bedc73c5188b75
Benjamin Zaslavsky <bzaslavsky@omnilog.fr>
2017-03-24 16:50:50 +0100

    Adding KahlanBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,7 @@ class AppKernel extends Kernel
             new Quantified\AggregatorBundle\AggregatorBundle(),
             new Quantified\GoogleBundle\GoogleBundle(),
             new Quantified\CoreBundle\CoreBundle(),
-//            new \HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
+            new Elephantly\KahlanBundle\KahlanBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
